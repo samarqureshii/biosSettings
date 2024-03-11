e0f85b62e3a2e38633b4196140dfb8f75ae2d1d7
samarqureshii <samarq713@gmail.com>
2024-03-11 09:37:51 -0400

    Initial commit

---
+++ processor/PWMcontrol.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <stdbool.h>
+#include <time.h>
+
+int PWMcontrol(float temperature){
+
+}
---
+++ processor/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <stdbool.h>
+#include <time.h>
+
+
+int main (void){
+
+    while(1){
+        
+    }
+
+ return 0;
+}

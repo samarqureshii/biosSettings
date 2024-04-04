9d5c46c89442ccd7c32512bb5c24fe161de6e03c
samarqureshii <samarq713@gmail.com>
2024-04-04 14:43:24 -0400

    Update main.c

--- de1-soc/main.c
+++ de1-soc/main.c
@@ -101,7 +101,7 @@ void PWMcontrol(unsigned int dutyCycle);
 void timerISR();
 void timerConfig(unsigned int duration);
 void interrupt_handler(void); 
-void adcRead();
+int adcRead();
 
 /********************************************************************************************************************************/
 /********************************************************GLOBAL VARIABLES********************************************************/
@@ -114,7 +114,7 @@ volatile int *GPIO_1 = (volatile int *)JP2_BASE;
 volatile int *SW = (volatile int *)SW_BASE;
 volatile int *gpio_direction_reg = (volatile int *)(JP2_BASE + 4);
 
-int temperature = 0;
+int globalTemp = 0;
 int RPM = 0; //fan speed in revolutions per min
 int usage = 0;
 int pwmHigh = 0;
@@ -145,10 +145,10 @@ int main (void){
     while(1){
         //read digital temperature via ADC
         *(ADC_ptr+1) = 0xFFFFFFFF;	// sets the ADC up to automatically perform conversions
-        adcRead(); //update the current temperature 
+        globalTemp = adcRead(); //update the current temperature 
 
         //generate PWM command and updated fan speed 
-        usage = (int)(((temperature - 25) * 100) / 105.0 + 0.5); //duty cycle as a percentage of possible temperature
+        usage = (int)(((globalTemp - 25) * 100) / 105.0 + 0.5); //duty cycle as a percentage of possible temperature
         PWMcontrol(usage);
         *LEDs = usage;
 
@@ -164,12 +164,12 @@ int main (void){
 /********************************************************HELPER FUNCTIONS*******************************************************/
 /*******************************************************************************************************************************/
 
-void adcRead(){ //read from the internal 12-bit ADC
+int adcRead(){ //read from the internal 12-bit ADC
     //scale / convert raw 12 bit ADC reading into integer temperature value (MAKE SURE COMPILE FLAGS ARE ON)
     int rawADC = *(ADC_ptr) & 0xFFF;
     float voltage = rawADC * (5.0 / 4095.0); //12 bit resolution
     float tempC = (voltage - 0.75) / (10.0 / 1000.0) + 25; //calibration
-    temperature = (int)(tempC + (tempC > 0 ? 0.5 : -0.5)) - 25; // maybe remove, no arduino
+    int temperature = (int)(tempC + (tempC > 0 ? 0.5 : -0.5)) - 25; // maybe remove, no arduino
 
     //temperature should not go above 3 digits, only use HEX2, HEX1, HEX0
     // bits 6 - 0 are HEX0 (ones place)
@@ -189,7 +189,8 @@ void adcRead(){ //read from the internal 12-bit ADC
 
 
     *HEX3_0 = (lookupTable[hundreds] << 16) | (lookupTable[tens] << 8) | lookupTable[ones];
-    *LEDs = temperature; //temporary debug 
+
+    return temperature;
 
 }
 
